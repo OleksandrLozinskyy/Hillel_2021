7bea9e13eb943e93b3312c6319b5a0c635b4ac28
Oleksandr Lozinskyi <lsash@gmail.com>
2021-12-31 00:31:03 +0200

    Add detail post view

--- Lesson23/core/urls.py
+++ Lesson23/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostView
 
 
 urlpatterns = [
-    path('', PostListView.as_view())
+    path('', PostListView.as_view()),
+    path('post/<int:pk>/', PostView.as_view())
 ]
--- Lesson23/core/views.py
+++ Lesson23/core/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.db import models
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostListView(ListView):
@@ -10,4 +11,11 @@ class PostListView(ListView):
         qs.append([post, post.like_set.filter(status=True).count, post.like_set.filter(status=False).count])
     queryset = qs
     template_name = "../core/posts.html"
-        
+
+class PostView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = "../core/post.html"    
+
+    def get_object(self, queryset=None):
+        return Post.objects.get(id=self.kwargs.get('pk'))
